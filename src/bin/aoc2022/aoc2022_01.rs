ad07217f6da58cebdae7c454b07bdabf4b09d042
HelpfulHelper <16364535+helpfulhelper@users.noreply.github.com>
2022-12-01 02:42:10 -0500

    Improved considerably, still needs some work

--- src/bin/aoc2022/aoc2022_01.rs
+++ src/bin/aoc2022/aoc2022_01.rs
@@ -1,29 +1,7 @@
 use aoc::read_lines2;
 
 pub struct AoC2022_01 {
-    data: Vec<Elf>,
-}
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Elf {
-    cals: u32,
-}
-
-impl std::fmt::Display for Elf {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.cals)
-    }
-}
-
-impl Elf {
-    pub fn new(c: Vec<String>) -> Self {
-        let mut t: u32 = 0;
-        for b in c {
-            t += b.parse::<u32>().unwrap();
-        }
-
-        Self { cals: t }
-    }
+    data: Vec<u32>,
 }
 
 impl AoC2022_01 {
@@ -35,19 +13,17 @@ impl AoC2022_01 {
 impl crate::Runner for AoC2022_01 {
     fn parse(&mut self) {
         let temp = read_lines2("input/2022-01.txt");
-        let mut t: Vec<String> = Vec::new();
+        let mut t: Vec<u32> = Vec::new();
         for x in temp {
-            if x != "" {
-                t.push(x);
+            if let Ok(y) = x.parse::<u32>() {
+                t.push(y)
             } else {
-                self.data.push(Elf::new(t.clone()));
+                self.data.push(t.iter().sum());
                 t.clear();
-            }
+            };
         }
 
         self.data.sort();
-        // print!("{:?}", self.data);
-        // print!("{:?}", x);
     }
 
     fn name(&self) -> (usize, usize) {
@@ -55,15 +31,12 @@ impl crate::Runner for AoC2022_01 {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        // crate::output(self.data)
-        crate::output(self.data.last().unwrap().cals)
+        crate::output(self.data.last().unwrap())
     }
 
     fn part2(&mut self) -> Vec<String> {
         crate::output(
-            self.data.pop().unwrap().cals
-                + self.data.pop().unwrap().cals
-                + self.data.pop().unwrap().cals,
+            self.data.pop().unwrap() + self.data.pop().unwrap() + self.data.pop().unwrap(),
         )
     }
 }
