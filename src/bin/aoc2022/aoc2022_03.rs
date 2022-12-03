a5d62268ccd7afe9aacc84140ea20c74a34f05a5
HelpfulHelper <16364535+helpfulhelper@users.noreply.github.com>
2022-12-03 11:56:59 -0500

    UGLY solution to 2022-03-p2

--- src/bin/aoc2022/aoc2022_03.rs
+++ src/bin/aoc2022/aoc2022_03.rs
@@ -1,9 +1,10 @@
 // use aoc::read_to_chars;
 use aoc::read_lines;
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 pub struct AoC2022_03 {
     data: Vec<char>,
+    badges: Vec<char>,
     scores: HashMap<char, usize>,
 }
 
@@ -11,16 +12,18 @@ impl AoC2022_03 {
     pub fn new() -> Self {
         Self {
             data: Vec::new(),
-            scores: AoC2022_03::gen_scores(),
+            badges: Vec::new(),
+            scores: gen_scores(),
         }
     }
-    fn gen_scores() -> HashMap<char, usize> {
-        let mut temp: HashMap<char, usize> = HashMap::new();
-        for (score, ch) in gen_alpha().iter().enumerate() {
-            temp.insert(ch.to_owned(), score + 1);
-        }
-        temp
+}
+
+fn gen_scores() -> HashMap<char, usize> {
+    let mut temp: HashMap<char, usize> = HashMap::new();
+    for (score, ch) in gen_alpha().iter().enumerate() {
+        temp.insert(ch.to_owned(), score + 1);
     }
+    temp
 }
 
 fn gen_alpha() -> Vec<char> {
@@ -29,8 +32,6 @@ fn gen_alpha() -> Vec<char> {
         .chain((b'A'..=b'Z').map(|c| c as char))
         .collect();
     return alpha.to_owned();
-    // let lower_alpha = (b'a'..b'z').map(|c| c as char).collect();
-    // let upper_alpha = (b'A'..b'Z').map(|c| c as char).collect();
 }
 
 impl crate::Runner for AoC2022_03 {
@@ -40,12 +41,19 @@ impl crate::Runner for AoC2022_03 {
             let (left, right) = l.split_at(l.len() / 2);
             let lset: HashSet<char> = left.chars().collect();
             let rset: HashSet<char> = right.chars().collect();
-            let dup = lset
-                .into_iter()
-                .filter(|s| rset.contains(s))
-                .collect::<String>();
-            // print!("{:?}", dup.chars().nth(0).unwrap());
+            let dup = lset.intersection(&rset).collect::<String>();
             self.data.push(dup.chars().nth(0).unwrap());
+        }
+
+        let lines2 = read_lines("../input/2022/03.txt");
+        let trio: Vec<Vec<String>> = lines2.chunks(3).map(|x| x.to_vec()).collect();
+        for t in trio {
+            let a: HashSet<char> = t[0].chars().collect();
+            let b: HashSet<char> = t[1].chars().collect();
+            let c: HashSet<char> = t[2].chars().collect();
+            let d1: HashSet<char> = a.intersection(&b).collect::<String>().chars().collect();
+            let d2 = d1.intersection(&c).collect::<String>();
+            self.badges.push(d2.chars().nth(0).unwrap());
         }
 
         // print!("{:?}", gen_alpha());
@@ -62,6 +70,11 @@ impl crate::Runner for AoC2022_03 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        crate::output("0".to_string())
+        let total: usize = self
+            .badges
+            .iter()
+            .map(|c| self.scores.get(c).unwrap())
+            .sum();
+        crate::output(total.to_string())
     }
 }
