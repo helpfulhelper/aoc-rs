8bcfb2d1addf62548cea686fdbbf3a3fa54e432f
HelpfulHelper <16364535+helpfulhelper@users.noreply.github.com>
2022-12-05 02:41:49 -0500

    2022_04 redo using structs and nom

--- src/bin/aoc2022/aoc2022_04.rs
+++ src/bin/aoc2022/aoc2022_04.rs
@@ -1,8 +1,33 @@
 use aoc::read_lines;
 use itertools::Itertools;
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::character::complete::digit1;
+use nom::combinator::map_res;
+use nom::sequence::separated_pair as sp;
+use nom::IResult;
+use std::str::FromStr;
 
 pub struct AoC2022_04 {
     data: Vec<Vec<u32>>,
+}
+
+struct Assignments {
+    left: (u32, u32),
+    right: (u32, u32),
+}
+
+impl Assignments {
+    fn parse(s: &str) -> Self {
+        let (_, (mut p1, mut p2)) =
+            sp(sp(pn, tag("-"), pn), tag(","), sp(pn, tag("-"), pn))(s).unwrap();
+        let (left, right) = if p1.0 < p2.0 { (p1, p2) } else { (p2, p1) };
+        Assignments { left, right }
+    }
+}
+
+fn pn(s: &str) -> IResult<&str, u32> {
+    map_res(digit1, u32::from_str)(s)
 }
 
 impl AoC2022_04 {
@@ -39,14 +64,14 @@ fn overlap(line: Vec<u32>) -> bool {
 
 impl crate::Runner for AoC2022_04 {
     fn parse(&mut self) {
-        for lines in read_lines("../input/2022/04.txt") {
-            self.data.push(
-                lines
-                    .split(|x| x == '-' || x == ',')
-                    .map(|n| n.parse::<u32>().unwrap())
-                    .collect::<Vec<u32>>(),
-            );
-        }
+        // for lines in read_lines("../input/2022/04.txt") {
+        //     self.data.push(
+        //         lines
+        //             .split(|x| x == '-' || x == ',')
+        //             .map(|n| n.parse::<u32>().unwrap())
+        //             .collect::<Vec<u32>>(),
+        //     );
+        // }
     }
 
     //year, day
