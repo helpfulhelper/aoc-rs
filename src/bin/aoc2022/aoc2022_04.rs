50b01ff71855459d1e563f08b40ef8f85e498c87
HelpfulHelper <16364535+helpfulhelper@users.noreply.github.com>
2022-12-05 03:00:37 -0500

    finished rewrite - so so so much better.

--- src/bin/aoc2022/aoc2022_04.rs
+++ src/bin/aoc2022/aoc2022_04.rs
@@ -1,15 +1,13 @@
 use aoc::read_lines;
-use itertools::Itertools;
-use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::digit1;
 use nom::combinator::map_res;
-use nom::sequence::separated_pair as sp;
+use nom::sequence::separated_pair;
 use nom::IResult;
 use std::str::FromStr;
 
 pub struct AoC2022_04 {
-    data: Vec<Vec<u32>>,
+    data: Vec<Assignments>,
 }
 
 struct Assignments {
@@ -19,14 +17,25 @@ struct Assignments {
 
 impl Assignments {
     fn parse(s: &str) -> Self {
-        let (_, (mut p1, mut p2)) =
-            sp(sp(pn, tag("-"), pn), tag(","), sp(pn, tag("-"), pn))(s).unwrap();
+        let (_, (p1, p2)) = separated_pair(
+            separated_pair(parse_num, tag("-"), parse_num),
+            tag(","),
+            separated_pair(parse_num, tag("-"), parse_num),
+        )(s)
+        .unwrap();
         let (left, right) = if p1.0 < p2.0 { (p1, p2) } else { (p2, p1) };
         Assignments { left, right }
     }
+    fn fully_contains(a: &&Assignments) -> bool {
+        (a.left.0 <= a.right.0 && a.right.1 <= a.left.1)
+            || (a.right.0 <= a.left.0 && a.left.1 <= a.right.1)
+    }
+    fn has_overlap(a: &&Assignments) -> bool {
+        a.left.1 >= a.right.0
+    }
 }
 
-fn pn(s: &str) -> IResult<&str, u32> {
+fn parse_num(s: &str) -> IResult<&str, u32> {
     map_res(digit1, u32::from_str)(s)
 }
 
@@ -36,42 +45,11 @@ impl AoC2022_04 {
     }
 }
 
-fn contains(line: Vec<u32>) -> bool {
-    let (lst, lend, rst, rend) = line.iter().next_tuple().unwrap();
-
-    if lst <= rst && rend <= lend {
-        return true;
-    } else if rst <= lst && lend <= rend {
-        return true;
-    }
-    false
-}
-
-fn overlap(line: Vec<u32>) -> bool {
-    let (lst, lend, rst, rend) = line.iter().next_tuple().unwrap();
-
-    if lst <= rst && rst <= lend {
-        return true;
-    } else if rst <= lst && lst <= rend {
-        return true;
-    } else if rst <= lend && lend <= rend {
-        return true;
-    } else if lst <= rend && rend <= lend {
-        return true;
-    }
-    false
-}
-
 impl crate::Runner for AoC2022_04 {
     fn parse(&mut self) {
-        // for lines in read_lines("../input/2022/04.txt") {
-        //     self.data.push(
-        //         lines
-        //             .split(|x| x == '-' || x == ',')
-        //             .map(|n| n.parse::<u32>().unwrap())
-        //             .collect::<Vec<u32>>(),
-        //     );
-        // }
+        for lines in read_lines("../input/2022/04.txt") {
+            self.data.push(Assignments::parse(&lines));
+        }
     }
 
     //year, day
@@ -80,22 +58,10 @@ impl crate::Runner for AoC2022_04 {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        let mut total = 0;
-        for l in &self.data {
-            if contains(l.to_vec()) {
-                total += 1;
-            }
-        }
-        crate::output(total)
+        crate::output(self.data.iter().filter(Assignments::fully_contains).count())
     }
 
     fn part2(&mut self) -> Vec<String> {
-        let mut total = 0;
-        for l in &self.data {
-            if overlap(l.to_vec()) {
-                total += 1;
-            }
-        }
-        crate::output(total)
+        crate::output(self.data.iter().filter(Assignments::has_overlap).count())
     }
 }
