8c5687e9d1ebccf2567c42059c9a93412a26709d
HelpfulHelper <16364535+helpfulhelper@users.noreply.github.com>
2022-12-06 09:04:16 -0500

    2022_06p1 was straightforward

---
+++ src/bin/aoc2022/aoc2022_06.rs
@@ -0,0 +1,55 @@
+use aoc::read_to_chars;
+use itertools::Itertools;
+// use aoc::read_lines;
+
+pub struct AoC2022_06 {
+    data: Vec<char>,
+}
+
+impl AoC2022_06 {
+    pub fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+}
+
+impl crate::Runner for AoC2022_06 {
+    fn parse(&mut self) {
+        self.data = read_to_chars("../input/2022/06.txt");
+    }
+
+    //year, day
+    fn name(&self) -> (usize, usize) {
+        (2022, 06)
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        let uniq = self
+            .data
+            .clone()
+            .windows(4)
+            .flat_map(<&[char; 4]>::try_from)
+            .find_map(|&a| {
+                if a.iter().unique().collect::<Vec<&char>>().len() == 4 {
+                    Some(a)
+                } else {
+                    None
+                }
+            })
+            .unwrap()
+            .into_iter()
+            .collect::<String>();
+        println!("{:?}", uniq);
+        let packets = self
+            .data
+            .clone()
+            .into_iter()
+            .collect::<String>()
+            .find(&uniq);
+        println!("{:?}", packets.unwrap());
+        crate::output(packets.unwrap() + 4)
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        crate::output("0".to_string())
+    }
+}
